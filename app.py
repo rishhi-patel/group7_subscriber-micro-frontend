32c01879d79ae27e5da2479e8344c8497b014171
Rich Hildred <rhildred@conestogac.on.ca>
2025-08-08 20:12:56 +0000

    leave port for app.py as 8080

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ async def create_subscriber(subscriber: Subscriber):
 app.mount("/", StaticFiles(directory="dist"), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8082, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
